d50e0d395032c4734daf69e3d8931047fa79ea3b
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2022-11-11 16:46:47 +0200

    Add files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import ContactForm from '../ContactForm/ContactForm';
+import Filter from '../Filter/Filter';
+import ContactList from '../ContactList/ContactList';
+import { Container, TitlePrimary, TitleSecondary } from './App.styled';
+
+const LOCAL_STORAGE_KEY = 'contacts';
+
+export default function App() {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  const formSubmitHandler = data => {
+    contacts.some(
+      ({ name }) => name.toLowerCase().trim() === data.name.toLowerCase().trim()
+    )
+      ? alert('This contact is already in list')
+      : setContacts([...contacts, data]);
+  };
+
+  const filterContact = () => {
+    if (contacts.length === 0) return;
+
+    const normalizedString = filter.toLowerCase().trim();
+    const filterContactList = contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedString)
+    );
+
+    return filterContactList;
+  };
+
+  const deleteContact = id => {
+    setContacts(prevState => {
+      prevState.filter(contact => contact.id !== id);
+      setFilter('');
+    });
+  };
+
+  return (
+    <Container>
+      <TitlePrimary>Phonebook</TitlePrimary>
+      <ContactForm onSubmit={formSubmitHandler} />
+
+      <TitleSecondary>Contacts</TitleSecondary>
+      <Filter
+        filter={filter}
+        onChange={event => setFilter(event.target.value)}
+      />
+      <ContactList contacts={filterContact()} deleteContact={deleteContact} />
+    </Container>
+  );
+}
